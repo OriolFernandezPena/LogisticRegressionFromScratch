60a76007646e09c32cc63dddedb24d583aace8a1
Oriol Fernandez Peña <oriol.fdez.penya@gmail.com>
2024-08-08 14:16:49 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+from src.model import LogisticRegression
+
+def main():
+    custom_clf = LogisticRegression()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/model/logistic_regression.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+class LogisticRegression():
+    def _init__(self):
+        pass
